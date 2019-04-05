da8ca969204a32b8e20a58eabeac2e7d20e58ce0
Anders <idris.maps@gmx.ch>
2019-04-05 09:27:52 +0200

    Draw map

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-import { prop } from 'ramda'
+import {
+  select,
+  geoMercator,
+  geoPath,
+} from 'd3'
+import {
+  WIDTH,
+  HEIGHT,
+} from './config'
+import drawRoads from './drawRoads'
+import drawAtm from './drawAtm'
 
-console.log(prop('a', {a:'hello'}))
+const body = select(document.body)
+const svg = body.append('svg')
+  .attr('viewBox', `0 0 ${WIDTH} ${HEIGHT}`)
+
+const projection = geoMercator()
+  .fitExtent(
+    [[0, 0], [WIDTH, HEIGHT]],
+    { type: 'Feature', properties: {}, geometry: { type: 'LineString', coordinates: [[6.64398, 46.77937], [6.64948, 46.78314]] } }
+  )
+const pathCreator = geoPath().projection(projection)
+
+drawRoads(svg, pathCreator)
+drawAtm(svg, projection)
